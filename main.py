48b6d39fb5a7ccf70a6f64bc821995a13492eda4
xxxx <xxxxxx@gmail.com>
2023-08-02 17:43:46 +0900

    [Add] index

--- main.py
+++ main.py
@@ -20,4 +20,6 @@ def speech_to_text(wav_filename):
 def speech_text(audio_np , wav_file_path: str , samplerate: int):
     sf.write(wav_file_path, audio_np, samplerate=samplerate)
     recognized_text = speech_to_text(wav_file_path)
-    return {"recognized_text": recognized_text}
+    # return {"recognized_text": recognized_text}
+    username = "あなたの名前は「" + username + "」です。"
+    return {"message": f"User: {username}"}
